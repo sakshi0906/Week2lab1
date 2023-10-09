fe8b142cae39f2d20397397920e029dc22d20008
Sami <sami.bencheikh@metropolia.fi>
2023-10-09 09:35:42 +0300

    Solved lab 1 week 2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express();
 const port = 3001;
-
+//Sam
 // Define your routes and handlers
 app.get("/", (req, res) => {
   res.send("Hello, World! This is a plain text response.");
